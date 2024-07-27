10eaf3024000e1eba75bea987a76cd625b8de429
Kushagra <VolodarOlathana@tutanota.com>
2024-07-27 13:15:49 +0530

    init(personal-website)

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './header.css'
+
+function Header() {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/blog">Blog</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Header;
